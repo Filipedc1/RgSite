8b498d223f00bca689c6884908ebc525eee2b295
fcatarcione91 <fcatarcione91@gmail.com>
2019-05-02 22:49:11 -0400

    Update images

---
+++ RgSite/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RgSite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RgSite.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        //public IEnumerable<Story> Stories { get; set; }
+    }
+}
