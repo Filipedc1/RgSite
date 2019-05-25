6cd0c1e9d2e56b6f7cea499276f90f091a440ecc
fcatarcione91 <fcatarcione91@gmail.com>
2019-05-25 15:14:42 -0400

    more updates

---
+++ RgSite/ViewModels/ProductViewModel.cs
@@ -0,0 +1,41 @@
+﻿using RgSite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RgSite.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId        { get; set; }
+        public string Name          { get; set; }
+        public string Description   { get; set; }
+        public string ImageUrl      { get; set; }
+
+        //public string PriceRange =>
+
+
+        public virtual IEnumerable<CustomerPrice> CustomerPrices    { get; set; }
+        public virtual IEnumerable<SalonPrice> SalonPrices          { get; set; }
+
+
+
+        //private string GetProductPriceRange(Product product)
+        //{
+        //    string range = string.Empty;
+
+              //need to use HttpContext to get user name or user role
+        //    if (App.UserRole == RoleType.Customer && product.CustomerPrices != null && product.CustomerPrices.Count() > 0)
+        //    {
+        //        range = $"${product.CustomerPrices.FirstOrDefault().Cost} - ${product.CustomerPrices.LastOrDefault().Cost}";
+        //    }
+        //    else if (App.UserRole == RoleType.Salon && product.SalonPrices != null && product.SalonPrices.Count() > 0)
+        //    {
+        //        range = $"${product.SalonPrices.FirstOrDefault()} - ${product.SalonPrices.LastOrDefault()}";
+        //    }
+
+        //    return range ?? "N/A";
+        //}
+    }
+}
